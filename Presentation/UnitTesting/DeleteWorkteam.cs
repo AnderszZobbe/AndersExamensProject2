225e777a88c7c7ecbfb7f5dfb4de71836a5adc25
Dupax <magnusj16@gmail.com>
2019-05-02 09:06:29 +0200

    I give up

---
+++ Presentation/UnitTesting/DeleteWorkteam.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Application_layer;
+using Domain;
+using System.Collections.Generic;
+using Application_layer.Exceptions;
+
+namespace UnitTesting
+{
+    [TestClass]
+    public class DeleteWorkteam
+    {
+        //[TestMethod]
+        //public void TestSuccesfulDeletion()
+        //{
+        //    Controller controller = Controller.Instance;
+        //    string foreman = "Hans";
+        //    controller.CreateWorkteam(foreman);
+
+        //    Workteam workteam = controller.GetWorkteamByName(foreman);
+        //    controller.DeleteWorkteam(workteam);
+        //    controller.GetWorkteamByName(foreman);
+        //    Assert.AreEqual(, controller.GetAllWorkteams());
+        //}
+
+        [TestMethod]
+        [ExpectedException(typeof(NotFoundException))]
+        public void ExpectedExceptionDeleteNonExistentWorkteam()
+        {
+            Controller controller = Controller.Instance;
+            string foreman = "Hans";
+
+            Workteam workteam = new Workteam(foreman);
+
+
+            controller.DeleteWorkteam(workteam);
+        }
+    }
+}
