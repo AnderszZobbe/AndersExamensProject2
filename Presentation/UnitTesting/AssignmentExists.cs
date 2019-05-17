3e19f51dbae30516110da9b5f6888b9dbae540cc
Dupax <magnusj11@hotmail.com>
2019-05-17 13:03:35 +0200

    Add Exsists tests

---
+++ Presentation/UnitTesting/AssignmentExists.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Application_layer;
+using Domain;
+using Persistence;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTesting
+{
+    [TestClass]
+    public class AssignmentExists
+    {
+        Controller controller;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            Controller.Connector = new DBTestConnector();
+            controller = Controller.Instance;
+        }
+
+        [TestMethod]
+        public void AssignementDoesExist()
+        {
+            Workteam workteam = controller.CreateWorkteam("AssignementDoesExist");
+            Order order = controller.CreateOrder(workteam, null, null, null, null, null, null, null, null, null, null, null);
+            Assignment assignment = controller.CreateAssignment(order, 1, Workform.Dag);
+            Assert.AreEqual(true, controller.AssignmentExists(assignment));
+        }
+
+        [TestMethod]
+        public void AssignmentDoesntExists()
+        {
+            Workteam workteam = controller.CreateWorkteam("AssignmentDoesntExists");
+            Order order = controller.CreateOrder(workteam, null, null, null, null, null, null, null, null, null, null, null);
+            Assignment assignment = controller.CreateAssignment(order, 1, Workform.Dag);
+            controller.DeleteAssignment(order, assignment);
+            Assert.AreEqual(false, controller.AssignmentExists(assignment));
+
+        }
+    }
+}
---
+++ Presentation/UnitTesting/OffDayExists.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Application_layer;
+using Domain;
+using Persistence;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTesting
+{
+
+    [TestClass]
+    public class OffDayExists
+    {
+        Controller controller;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            Controller.Connector = new DBTestConnector();
+            controller = Controller.Instance;
+        }
+
+        [TestMethod]
+        public void OffDayDoesExist()
+        {
+            Workteam workteam = controller.CreateWorkteam("OffDayDoesExist");
+            Offday offday = controller.CreateOffday(workteam, OffdayReason.Helligdag, DateTime.Today, 5);
+            Assert.AreEqual(true, controller.OffdayExists(offday));
+        }
+
+        [TestMethod]
+        public void OffDayDoesntExists()
+        {
+            Workteam workteam = controller.CreateWorkteam("OffDayDoesntExist");
+            Offday offday = controller.CreateOffday(workteam, OffdayReason.Helligdag, DateTime.Today, 5);
+            controller.DeleteOffday(workteam, offday);
+            Assert.AreEqual(false, controller.OffdayExists(offday));
+        }
+    }
+}
